c9ac1e6d01e25bdb0cc2a44b51357c8c707d2dc7
Kyle Cooper <kylcooper22@gmail.com>
2015-11-08 15:08:02 -0500

    fixes for migration and more styling

--- note_app/note_app/db/migrate/20151105222854_create_join_table_user_projects.rb
+++ note_app/note_app/db/migrate/20151105222854_create_join_table_user_projects.rb
@@ -1,5 +1,7 @@
 class CreateJoinTableUserProjects < ActiveRecord::Migration
   def change
+  	drop_table :projects_users
+  	
   	create_join_table :projects, :users do |t|
     	t.integer :project_id
     	t.integer :user_id
--- note_app/note_app/db/migrate/20151106181959_add_has_many_tasks_to_project_model.rb
+++ note_app/note_app/db/migrate/20151106181959_add_has_many_tasks_to_project_model.rb
@@ -1,5 +1,7 @@
 class AddHasManyTasksToProjectModel < ActiveRecord::Migration
   def change
+  	remove_column :tasks, :project_id
+
   	add_reference :tasks, :project, index: true 
   end
 end
